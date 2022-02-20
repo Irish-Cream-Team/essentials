4540e6662fab029241e3c71e22b6bf8873e9d3bb
Ubuntu <shoval@shoval.ozowjmdqmnrudbq34qvbmdmvab.fx.internal.cloudapp.net>
2022-02-20 12:56:41 +0000

    delete script

--- script.sh
+++
@@ -1,9 +0,0 @@
-ls -l
-success_status=0
-for test_dir in `find ./test/values/* -type d` ;do      
-    for values_files in `find $test_dir -name '*.yaml'`; do
-        dir_name=`find $test_dir -type d -printf "%f\n"`
-        file_name=`find $values_files -exec basename {} .yaml \;`
-        helm template essentials ./ -f $values_files --output-dir ./test/output/$dir_name/$file_name
-    done
-done
