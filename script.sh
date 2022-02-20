52016ae2bcc12b3a4a0952bd0b47ba1462ed55fb
Ubuntu <shoval@shoval.ozowjmdqmnrudbq34qvbmdmvab.fx.internal.cloudapp.net>
2022-02-20 12:00:48 +0000

    add test output

--- script.sh
+++ script.sh
@@ -1,12 +1,9 @@
 ls -l
 success_status=0
-
-for test_dir in `find ./test-values/* -type d` ;do      
-    
+for test_dir in `find ./test/values/* -type d` ;do      
     for values_files in `find $test_dir -name '*.yaml'`; do
         dir_name=`find $test_dir -type d -printf "%f\n"`
         file_name=`find $values_files -exec basename {} .yaml \;`
-        echo $dir_name/$file_name
-        # echo $values_files
+        helm template essentials ./ -f $values_files --output-dir ./test/output/$dir_name/$file_name
     done
 done
